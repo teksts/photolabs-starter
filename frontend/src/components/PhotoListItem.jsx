16730ea5bf86e439f3b29f0e1f8380d4041825da
teksts <mark.egelhoff1@gmail.com>
2023-07-12 14:55:32 -0700

    Initial commit
    Implement PhotoListItem with default props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,9 +3,21 @@ import React from 'react';
 
 import '../styles/PhotoListItem.scss';
 
-const PhotoListItem = () => {
-  /* Insert React */
-}
+const PhotoListItem = (props) => {
+  return (
+    <div className='photo-list__item'>
+      <img className='photo-list__image' src={props.imageSource}></img>
+      <div className='photo-list__user-details photo-list__user-info'>
+        <div className='photo-list__user-info'>
+          {props.username}
+        </div>
+        <div className='photo-list__user-location'>
+          {props.location.city}, {props.location.country}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 PhotoListItem.defaultProps = {
   "id": "1",
